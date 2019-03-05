01a7173e61c1abd68b894a900093ec325c76c386
mkhelifa <khlf.mehdi@gmail.com>
2019-03-05 16:53:41 +0100

    Add interval.includes method

--- src/interval.test.js
+++ src/interval.test.js
@@ -5,7 +5,17 @@ describe('overlaps', function() {
     [new Interval(1, 3), new Interval(4, 7), false],
     [new Interval(2, 8), new Interval(3, 7), true],
     [new Interval(0, 8), new Interval(8, 10), false]
-  ])('Overlaps %s', (inter1, inter2, expected) => {
+  ])('Overlaps %s &  %s', (inter1, inter2, expected) => {
     expect(inter1.overlaps(inter2)).toBe(expected);
+  });
+});
+
+describe('includes', function() {
+  test.each([
+    [new Interval(1, 3), new Interval(4, 7), false],
+    [new Interval(2, 8), new Interval(3, 7), true],
+    [new Interval(0, 8), new Interval(8, 10), true]
+  ])('Includes %s %s', (inter1, inter2, expected) => {
+    expect(inter1.includes(inter2)).toBe(expected);
   });
 });
