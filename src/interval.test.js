674cdc215b0edecac6e3373cca4429290354aa5f
mkhelifa <khlf.mehdi@gmail.com>
2019-03-05 16:54:52 +0100

    fix src/interval.test.js

--- src/interval.test.js
+++ src/interval.test.js
@@ -14,7 +14,7 @@ describe('includes', function() {
   test.each([
     [new Interval(1, 3), new Interval(4, 7), false],
     [new Interval(2, 8), new Interval(3, 7), true],
-    [new Interval(0, 8), new Interval(8, 10), true]
+    [new Interval(0, 8), new Interval(8, 10), false]
   ])('Includes %s %s', (inter1, inter2, expected) => {
     expect(inter1.includes(inter2)).toBe(expected);
   });
